368f3c6c952cf737835cd46eff191d04992ed933
jaeminjung <jaeminjung@hotmail.com>
2018-07-03 19:38:16 +0200

    setting change

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^k)cwx6c_u42v=f@(+^pgt^*jmb=g05my6ouc%wqjazi-kncd4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jaeminjung.pythonanywhere.com', 'localhost',
                  '.pythonanywhere.com', '127.0.0.1']
